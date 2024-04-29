7a35007808d2ca4e24adccffbe62b7ef97d9d18a
MONTH4 <goei4559@gmail.com>
2024-04-29 16:51:14 +0900

    [feat] db 재구성
    [feat] db 재구성

--- services/web/main.py
+++ services/web/main.py
@@ -8,12 +8,16 @@ db = SQLAlchemy(app)
 
 class Darkweb(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), nullable=False)
+    domain = db.Column(db.String(100), nullable=False)
+    url = db.Column(db.String(200), nullable=False)
+    depth = db.Column(db.Integer, nullable=False)
+    parameter = db.Column(db.String(200), nullable=True)  # 파라미터는 선택적이라 가정
+    title = db.Column(db.String(200), nullable=False)
+    words = db.Column(db.String(1000), nullable=False)  # 저장된 단어 목록
 
 @app.before_request
 def init_db():
+    # db.drop_all()  # 기존 데이터베이스의 모든 테이블 삭제, 다시 재구성할때만 실행
     db.create_all()
     print("Database initialized successfully!")
 
