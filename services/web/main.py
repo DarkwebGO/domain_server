8bc86980d86b74d6f669ad9bdb8feb2de880ba05
MONTH4 <goei4559@gmail.com>
2024-04-29 17:11:53 +0900

    [chore] 테이블 삭제 기능 주석화
    [chore] 테이블 삭제 기능 주석화

--- services/web/main.py
+++ services/web/main.py
@@ -14,10 +14,11 @@ class Darkweb(db.Model):
     parameter = db.Column(db.String(200), nullable=True)  # 파라미터는 선택적이라 가정
     title = db.Column(db.String(200), nullable=False)
     words = db.Column(db.String(1000), nullable=False)  # 저장된 단어 목록
+    html_content_path = db.Column(db.String(200), nullable=True)  # HTML 파일의 저장 경로
 
 @app.before_request
 def init_db():
-    # db.drop_all()  # 기존 데이터베이스의 모든 테이블 삭제, 다시 재구성할때만 실행
+    ## db.drop_all()  # 기존 데이터베이스의 모든 테이블 삭제, 다시 재구성할때만 실행
     db.create_all()
     print("Database initialized successfully!")
 
